a2ebd83ce3f8e23c2a7ef8ce58c19e1f3f2783bc
Eugenio Guidetti <Eugenio Guidetti@10.169.163.132>
2013-01-14 11:23:27 +0100

    Portata a buon punto l'implementazione della servlet registrazione. Creata la pagina registrazione.jsp.

--- SWIMv2Client/src/servlet/Registrazione.java
+++ SWIMv2Client/src/servlet/Registrazione.java
@@ -1,17 +1,20 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import entity.Abilita;
 import session.GestoreAbilitaRemote;
-import session.GestoreProfiloRemote;
 import session.GestoreUserRemote;
 import utility.Comunicazione;
 import utility.Messaggio;
@@ -32,20 +35,24 @@ public class Registrazione extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
 			Context context = new InitialContext();
-			GestoreProfiloRemote gestoreProfilo = (GestoreProfiloRemote) context.lookup("GestoreProfilo/remote");
+			RequestDispatcher dispatcher;
+			GestoreUserRemote gestoreUser = (GestoreUserRemote) context.lookup("GestoreUser/remote");
 			String email = (String) request.getAttribute("rEmail");
 			Pattern pattern = Pattern.compile(Comunicazione.EMAIL_PATTERN);
 			Matcher matcher = pattern.matcher(email);
 			if(!matcher.find()) {
 				Messaggio messaggio = new Messaggio(TipoMessaggio.ERRORE, Comunicazione.EMAIL_NON_VALIDA);
-				// inoltrare il messaggio alla Home Page;
+				request.setAttribute("messaggio", messaggio);
+				dispatcher = request.getRequestDispatcher("index.jsp");
+				dispatcher.forward(request, response);
 			}
 			String nickname = (String) request.getAttribute("rNickname");
-			/*if(!gestoreProfilo.controllaDisponibilitaNickname(nickname)) {
+			if(!gestoreUser.controllaDisponibilitaNickname(nickname)) {
 				Messaggio messaggio = new Messaggio(TipoMessaggio.AVVISO, Comunicazione.NICKNAME_NON_LIBERO);
-				// inoltrare il messaggio alla Home Page;
-			}*/
-			GestoreUserRemote gestoreUser = (GestoreUserRemote) context.lookup("GestoreUser/remote");
+				request.setAttribute("messaggio", messaggio);
+				dispatcher = request.getRequestDispatcher("index.jsp");
+				dispatcher.forward(request, response);
+			}
 			String nome = (String) request.getAttribute("rNome");
 			String cognome = (String) request.getAttribute("rCognome");
 			String sesso = (String) request.getAttribute("rSesso");
@@ -54,12 +61,12 @@ public class Registrazione extends HttpServlet {
 			String password = (String) request.getAttribute("rPassword");
 			String avatar = "";
 			gestoreUser.registra(nickname, password, email, nome, cognome, avatar, citta, sesso, annoNascita);
-			/* inltrare il nickname alla prossima pagina jsp */
+			request.setAttribute("nickname", nickname);
 			GestoreAbilitaRemote gestoreAbilita = (GestoreAbilitaRemote) context.lookup("GestoreAbilita/remote");
-			/* recuperare le abilita' disponibili nel sistema */
-			/* inoltrare tutti i parametri ricevuti alla prossima pagina jsp */
-			/* inoltrare la lista delle abilita' recuperate alla prossima pagina jsp */
-			/* chiamare la prossima pagina jsp */
+			List<Abilita> abilitaDisponibili = gestoreAbilita.recuperaAbilitaSistema();
+			request.setAttribute("abilitaDisponibili", abilitaDisponibili);
+			dispatcher = request.getRequestDispatcher("registrazione.jsp");
+			dispatcher.forward(request, response);
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
